a24b9bb4ef1fe89a4d3ee7bcdb5fe95b86932c30
Ben Hunter <bkhunter@ualberta.ca>
2016-02-27 16:11:33 -0700

    Passing freetests!

--- server.py
+++ server.py
@@ -74,27 +74,37 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect("/static/index.html")
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+    data =  flask_post_json()
+
+    if request.method == 'PUT':
+        myWorld.set(entity, data)
+    elif request.method == "POST":
+        for item in data.keys():
+            myWorld.update(entity,item,data[item])
+
+    res = myWorld.get(entity)
+    return json.dumps(res)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world())
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")    # entity is just a variable  
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    ent = myWorld.get(entity)
+    return flask.jsonify(ent)
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
-    '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return "cleared!"
+    
 
 if __name__ == "__main__":
     app.run()
